6cdb2858e712f2f7366b5529a0039a7f1ae1796e
Shea Craig <sheagcraig@gmail.com>
2020-06-17 15:52:53 -0400

    Remove redundant test handler func.

--- internal/escrow/escrow_test.go
+++ internal/escrow/escrow_test.go
@@ -20,126 +20,68 @@ func TestPostCryptServer(t *testing.T) {
 		Username:  "tester",
 	}
 
-	mockCryptServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-
-		if r.Method != "POST" {
-			t.Errorf("expected 'POST' request, got '%s'", r.Method)
-		}
-
-		if r.URL.EscapedPath() != "/checkin/" {
-			t.Errorf("expected request to '/checkin', got '%s'", r.URL.EscapedPath())
-		}
-
-		authusername, authpassword, _ := r.BasicAuth()
-		if authusername != "" || authpassword != "" {
-			t.Errorf("expected no basic auth got %q:%q", authusername, authpassword)
-		}
-
-		// cryptserver expects the following form data recovery_password, serial,
-		// macname, username
-		// see: https://github.com/grahamgilbert/Crypt-Server/blob/master/server/views.py#L442
-		r.ParseForm()
-		actual := r.Form.Get("recovery_password")
-		if actual != expected.Pass {
-			t.Errorf("expected 'recovery_password=%v' got %v", expected.Pass, actual)
-		}
-
-		actual = r.Form.Get("serial")
-		if actual != expected.Serialnum {
-			t.Errorf("expected 'serial=%v' got %v", expected.Serialnum, actual)
-		}
-
-		actual = r.Form.Get("macname")
-		if actual != expected.Hostname {
-			t.Errorf("expected 'macname=%v' got %v", expected.Hostname, actual)
-		}
-
-		actual = r.Form.Get("username")
-		if actual != expected.Username {
-			t.Errorf("expected 'username=%v' got %v", expected.Username, actual)
-		}
-	}))
-	defer mockCryptServer.Close()
-
-	endpoint := CryptServerInfo{
-		Server: mockCryptServer.URL,
-		URI:    "/checkin/",
-	}
-
-	resp, err := expected.PostCryptServer(endpoint)
-	if err != nil {
-		t.Errorf("errored posting escrow data to mock cryptserver with %v", err)
-	}
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("errored posting escrow data to mock cryptserver expected 200 got %v", resp.StatusCode)
-	}
-}
-
-
-func TestPostBasicAuthCryptServer(t *testing.T) {
-	expected := CryptServerData{
-		Pass:      "2345.1234.6566.foo",
-		Serialnum: "1234foobar",
-		Hostname:  "testing.example.com",
-		Username:  "tester",
-	}
 	basicauth := []string{"DarthHelmet", "12345"}
-
-	mockCryptServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-
-		if r.Method != "POST" {
-			t.Errorf("expected 'POST' request, got '%s'", r.Method)
-		}
-
-		if r.URL.EscapedPath() != "/checkin/" {
-			t.Errorf("expected request to '/checkin', got '%s'", r.URL.EscapedPath())
-		}
-
-		authusername, authpassword, _ := r.BasicAuth()
-		if authusername != basicauth[0] || authpassword != basicauth[1] {
-			t.Errorf("expected username:password=%q, got '%s:%s'", basicauth, authusername, authpassword)
-		}
-
-		// cryptserver expects the following form data recovery_password, serial,
-		// macname, username
-		// see: https://github.com/grahamgilbert/Crypt-Server/blob/master/server/views.py#L442
-		r.ParseForm()
-		actual := r.Form.Get("recovery_password")
-		if actual != expected.Pass {
-			t.Errorf("expected 'recovery_password=%v' got %v", expected.Pass, actual)
-		}
-
-		actual = r.Form.Get("serial")
-		if actual != expected.Serialnum {
-			t.Errorf("expected 'serial=%v' got %v", expected.Serialnum, actual)
-		}
-
-		actual = r.Form.Get("macname")
-		if actual != expected.Hostname {
-			t.Errorf("expected 'macname=%v' got %v", expected.Hostname, actual)
-		}
-
-		actual = r.Form.Get("username")
-		if actual != expected.Username {
-			t.Errorf("expected 'username=%v' got %v", expected.Username, actual)
-		}
-	}))
-	defer mockCryptServer.Close()
-
-	endpoint := CryptServerInfo{
-		Server: mockCryptServer.URL,
-		URI:    "/checkin/",
-		Username: basicauth[0],
-		Password: basicauth[1],
+	tests := []struct {
+		name string
+		username string
+		password string
+	}{
+		{name: "No auth escrow"},
+		{name: "Basic auth escrow", username: basicauth[0], password: basicauth[1]},
 	}
 
-	resp, err := expected.PostCryptServer(endpoint)
-	if err != nil {
-		t.Errorf("errored posting escrow data to mock cryptserver with %v", err)
-	}
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("errored posting escrow data to mock cryptserver expected 200 got %v", resp.StatusCode)
+	for _, tc := range tests {
+		mockCryptServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+
+			if r.Method != "POST" {
+				t.Errorf("expected 'POST' request, got '%s'", r.Method)
+			}
+
+			if r.URL.EscapedPath() != "/checkin/" {
+				t.Errorf("expected request to '/checkin', got '%s'", r.URL.EscapedPath())
+			}
+
+			authusername, authpassword, _ := r.BasicAuth()
+			if authusername != tc.username || authpassword != tc.password {
+				t.Errorf("expected '%s:%s' got '%s:%s'", tc.username, tc.password, authusername, authpassword)
+			}
+
+			// cryptserver expects the following form data recovery_password, serial,
+			// macname, username
+			// see: https://github.com/grahamgilbert/Crypt-Server/blob/master/server/views.py#L442
+			r.ParseForm()
+			actual := r.Form.Get("recovery_password")
+			if actual != expected.Pass {
+				t.Errorf("expected 'recovery_password=%v' got %v", expected.Pass, actual)
+			}
+
+			actual = r.Form.Get("serial")
+			if actual != expected.Serialnum {
+				t.Errorf("expected 'serial=%v' got %v", expected.Serialnum, actual)
+			}
+
+			actual = r.Form.Get("macname")
+			if actual != expected.Hostname {
+				t.Errorf("expected 'macname=%v' got %v", expected.Hostname, actual)
+			}
+
+			actual = r.Form.Get("username")
+			if actual != expected.Username {
+				t.Errorf("expected 'username=%v' got %v", expected.Username, actual)
+			}
+		}))
+		defer mockCryptServer.Close()
+
+		endpoint := CryptServerInfo{Server: mockCryptServer.URL, URI: "/checkin/", Username: tc.username, Password: tc.password}
+		t.Run(tc.name, func(t *testing.T) {
+			resp, err := expected.PostCryptServer(endpoint)
+			if err != nil {
+				t.Errorf("errored posting escrow data to mock cryptserver with %v", err)
+			}
+			if resp.StatusCode != http.StatusOK {
+				t.Errorf("errored posting escrow data to mock cryptserver expected 200 got %v", resp.StatusCode)
+			}
+		})
 	}
 }
